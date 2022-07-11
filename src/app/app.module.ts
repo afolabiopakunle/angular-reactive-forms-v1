bd1ab6c12f21625d5550a06bb17009dff5b2d1f5
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-11 18:15:08 +0100

    reactive form created and hooked to template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, ReactiveFormsModule ],
   declarations: [ AppComponent, SignUpFormComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpFormComponent implements OnInit {
 
+  form = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl()
+  })
+  
   constructor() { }
 
   ngOnInit() {
