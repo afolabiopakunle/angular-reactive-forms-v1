c1b3227d339ceed2508890f0df9fbfaba4328f85
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-11 18:03:25 +0100

    sign-up form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SignUpFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
